f1c84fc3664e426ad49885e450fbae55267bfc78
unknown <kvitriseptia@gmail.com>
2025-12-29 21:08:59 +0700

    fix: add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: '/Recipedia/',
+  
 })
